ee301a01e90f15dbe8a169c771fd4d6cdb801082
kush5900 <nsanzudaniel59@gmail.com>
2017-03-29 22:20:21 +0200

    create popular posts

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {PopularpostsService} from '../../services/popularposts.service';
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  providers:[PopularpostsService]
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
+  posts = [];
+  constructor(private activatedRoute: ActivatedRoute,private popular: PopularpostsService) {
+        popular.getPosts().subscribe(posts =>{
+        this.posts = posts;
+        console.log(posts);
+      });
+  }
 
   ngOnInit() {
   }
